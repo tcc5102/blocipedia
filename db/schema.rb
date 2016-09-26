5473798762a8a8c026acee9f16b1d76c34889bb3
Tyler Cooper <tyler.cooper91@gmail.com>
2016-09-26 17:54:43 -0400

    user sign up

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,12 @@ ActiveRecord::Schema.define(version: 20160925213033) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
