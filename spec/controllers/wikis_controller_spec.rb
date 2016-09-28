7004eb68587c8739843c138604d7d72b3f648aaa
Tyler Cooper <tyler.cooper91@gmail.com>
2016-09-28 13:44:23 -0400

    crud setup for wiki

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -23,15 +23,20 @@ RSpec.describe WikisController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    it "instantiates @post" do
+    it "renders the #new view" do
+        get :new
+        expect(response).to render_template :new
+      end
+
+    it "instantiates @wiki" do
         get :new
         expect(assigns(:wiki)).not_to be_nil
       end
     end
 
   describe "WIKI create" do
-    it "increases the number of Wikis by 1" do
-      expect{wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+    it "increases the number of Wiki by 1" do
+      expect{wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Wiki,:count).by(1)
     end
 
     it "assigns the new wiki to @wiki" do
@@ -42,6 +47,28 @@ RSpec.describe WikisController, type: :controller do
     it "redirects to the new wiki" do
       wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
       expect(response).to redirect_to Wiki.last
+    end
+  end
+
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, {id: my_wiki.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, {id: my_wiki.id}
+      expect(response).to render_template :edit
+    end
+
+    it "assigns wiki to be updated to @wiki" do
+      get :edit, {id: my_wiki.id}
+
+      wiki_instance = assigns(:wiki)
+
+      expect(wiki_instance.id).to eq my_wiki.id
+      expect(wiki_instance.title).to eq my_wiki.title
+      expect(wiki_instance.body).to eq my_wiki.body
     end
   end
 end
